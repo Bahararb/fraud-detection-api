20d780ddb541660350bf127d404fcb42dc74beb7
bahar <barabalanjari@gmail.com>
2026-06-19 11:38:26 -0400

    Clean repo and update API + gitignore

--- app/main.py
+++ app/main.py
@@ -7,7 +7,10 @@ import os
 
 app = FastAPI(title="Fraud Detection API")
 
+# API KEY 
 API_KEY = "fraud123"
+
+# Load model globally
 model = None
 
 
@@ -19,6 +22,7 @@ def load_model():
     model_path = os.path.join(base_dir, "model", "fraud_model.pkl")
 
     model = joblib.load(model_path)
+    print("Model loaded successfully")
 
 
 
@@ -55,64 +59,43 @@ class Transaction(BaseModel):
     Amount: float
 
 
-
 @app.get("/")
 def home():
     return {"message": "Fraud Detection API is running"}
 
 
+
 @app.post("/predict")
 def predict(data: Transaction, x_api_key: str = Header(None)):
 
-    # API key check
+   
     if x_api_key != API_KEY:
         raise HTTPException(status_code=403, detail="Invalid API Key")
 
     try:
-    
-        input_data = pd.DataFrame([{
-            "Time": data.Time,
-            "V1": data.V1,
-            "V2": data.V2,
-            "V3": data.V3,
-            "V4": data.V4,
-            "V5": data.V5,
-            "V6": data.V6,
-            "V7": data.V7,
-            "V8": data.V8,
-            "V9": data.V9,
-            "V10": data.V10,
-            "V11": data.V11,
-            "V12": data.V12,
-            "V13": data.V13,
-            "V14": data.V14,
-            "V15": data.V15,
-            "V16": data.V16,
-            "V17": data.V17,
-            "V18": data.V18,
-            "V19": data.V19,
-            "V20": data.V20,
-            "V21": data.V21,
-            "V22": data.V22,
-            "V23": data.V23,
-            "V24": data.V24,
-            "V25": data.V25,
-            "V26": data.V26,
-            "V27": data.V27,
-            "V28": data.V28,
-            "Amount": data.Amount
-        }])
 
-        # prediction
+      
+        input_data = pd.DataFrame([data.dict()])
+
+        # (Optional debug)
+        print("INPUT:", input_data)
+
         prediction = model.predict(input_data)[0]
+        print("PREDICTION:", prediction)
 
-        probability = None
+       
         if hasattr(model, "predict_proba"):
-            probability = model.predict_proba(input_data)[0][1]
+            proba = model.predict_proba(input_data)[0]
+            probability = float(proba[1])
+        else:
+            probability = 0.0
 
+        print("PROBABILITY:", probability)
+
+        
         return {
             "prediction": int(prediction),
-            "fraud_probability": float(probability) if probability is not None else None
+            "fraud_probability": round(probability, 4)
         }
 
     except Exception as e:
